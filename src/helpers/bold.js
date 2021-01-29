cbeccb6fb60257654b9a7daca3d5fa3062868fbe
Jahanban <jahanban@gmail.com>
2021-01-29 18:10:13 -0500

    Creating another helper mainButton.js

--- src/helpers/bold.js
+++ src/helpers/bold.js
@@ -1,4 +1,4 @@
 module.exports = function (options) {
-    var bolder = "<strong>" + options.fn(this) + "</strong>";
+    var bolder = `<strong> ${options.fn(this)}</strong>`;
     return bolder;
 }
